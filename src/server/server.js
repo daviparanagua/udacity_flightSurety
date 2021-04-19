56f001bf82308b4ddd9e3113c06f75e20904132d
Davi Paranaguá Vital <daviparanagua@yahoo.com.br>
2021-04-19 08:07:47 -0300

    Oracle registration

--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,4 @@
+import "babel-polyfill";
 import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
 import flights from './flights.js';
 import Config from './config.json';
@@ -9,33 +10,61 @@ import cors from 'cors';
 let config = Config['localhost'];
 let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
 web3.eth.defaultAccount = web3.eth.accounts[0];
+let owner = web3.eth.accounts[0];
 let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
 
-
-flightSuretyApp.events.OracleRequest({
-    fromBlock: 0
-  }, function (error, event) {
-    if (error) console.log(error)
-    console.log(event)
-});
+let oracles = [];
 
 const app = express();
 app.use(cors());
 
+async function start() {
+  await initializeOracles();
+  startExpress();
+}
 
-app.get('/api', (req, res) => {
-    res.send({
-      message: 'An API for use with your Dapp!'
-    })
-})
 
-app.get('/api/flights', (req, res) => {
-  res.send({
-    status: 'OK',
-    flights
+async function initializeOracles() {
+      let allAccounts = await web3.eth.getAccounts()
+      let oracleAccounts = allAccounts.slice(20,40); 
+
+      let fee = await flightSuretyApp.methods.REGISTRATION_FEE().call()
+
+      console.log(`Registering ${oracleAccounts.length} oracles...`)
+        oracleAccounts.forEach(async account => {
+          await flightSuretyApp.methods.registerOracle().send({
+              from: account,
+              value: fee,
+              gas: 9999999
+          })
+          
+          let indexes = await flightSuretyApp.methods.getMyIndexes().call({
+                  from: account
+              })
+
+          let oracle = {indexes, account}
+
+          oracles.push(oracle);
+          console.log(`Oracle Registered: ${oracle.indexes} points to ${oracle.account}`);
+      });
+}
+
+function startExpress() {
+  app.get('/api', (req, res) => {
+      res.send({
+        message: 'An API for use with your Dapp!'
+      })
   })
-})
 
+  app.get('/api/flights', (req, res) => {
+    res.send({
+      status: 'OK',
+      flights
+    })
+  })
+}
+
+start();
 export default app;
 
 
